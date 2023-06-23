3c20f3944e6c9662c4893edf1260f003390adae2
siddhantc1999 <schiring369@gmail.com>
2023-06-23 09:18:36 +0530

    commit

---
+++ Assets/Assets/Free Pixel Art Forest/Demo/MoveBackground.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour {
+
+
+
+	public float speed;
+	private float x;
+	public float PontoDeDestino;
+	public float PontoOriginal;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		//PontoOriginal = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+		x = transform.position.x;
+		Debug.Log("the x"+x);
+		x += speed * Time.deltaTime;
+		transform.position = new Vector3 (x, transform.position.y, transform.position.z);
+
+
+
+		if (x <= PontoDeDestino){
+
+			Debug.Log ("hhhh");
+			x = PontoOriginal;
+			transform.position = new Vector3 (x, transform.position.y, transform.position.z);
+		}
+
+
+	}
+}
